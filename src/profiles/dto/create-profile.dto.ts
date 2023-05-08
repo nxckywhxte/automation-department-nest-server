dd229249ba819a4d850120d90b5f6a4d62775710
Nikita Belonogov <crcku.mark00@gmail.com>
2023-05-08 11:33:24 +0300

    add global validation pipe, add validation to entities

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,10 +1,16 @@
 import { Group } from '../../groups/entities/group.entity';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateProfileDto {
+  @IsNotEmpty({ message: 'Обязательное поле' })
   username: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
   lastName: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
   firstName: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
   patronymic: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
   birthday: Date;
   groups: Group[];
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
 import { Role } from '../../roles/entities/role.entity';
 import { Profile } from '../../profiles/entities/profile.entity';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsEmail({}, { message: 'Неверный формат электронного адреса' })
   email: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @MaxLength(18, { message: 'Пароль может содержать до 18 символов' })
+  @MinLength(8, { message: 'Пароль должен содержать более 8 символов' })
   password: string;
+  @IsNotEmpty({ message: 'Обязательное поле' })
   roles: Role[];
   profile: Profile;
 }
