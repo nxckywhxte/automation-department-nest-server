4440b7f5475d73f731d30bb8352e445c852aa8f8
Nikita Belonogov <crcku.mark00@gmail.com>
2023-05-14 00:23:55 +0300

    add roles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
     }),
     UsersModule,
+    RolesModule,
   ],
   controllers: [],
   providers: [],
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'username', type: 'string', unique: true, nullable: false })
+  username: string;
+
+  @Column({ name: 'email', type: 'string', unique: true, nullable: false })
+  email: string;
+
+  @Column({ name: 'hashed_password', type: 'string', nullable: false })
+  hashedPassword: string;
+}
